12f2d3f1c8ac47145025851a26dcfc768b71e7d2
Luan Prais <luanp.praislle@gmail.com>
2024-09-12 13:19:21 -0300

    Condicionais (switch)

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+//1 estrutura de repetição
+//const start = () => {
+    //let count = 0
+    //while(count <= 10){
+        //console.log(count)
+       // count = count + 1
+   // }
+//}
+
+//start()
+
+//2 Condicional (switch)
+
 const start = () => {
-    let count = 0
-    while(count <= 10){
-        console.log(count)
-        count = count + 1
-    }
+    
+    while(true){
+       let opção = "sair"
+       switch(opção) {
+            case "cadastrar": 
+            console.log("vamos cadastrar")  
+            break
+        case "listar":
+            console.log("vamos listar")
+            break
+        case "sair":
+            return
+     }
+  }
+
 }
 
 start()
