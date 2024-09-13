a37522475ac5ebdad9b508367d383d1ae41f63d7
Luan Prais <luanp.praislle@gmail.com>
2024-09-13 19:17:30 -0300

    Listar metas

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 
 //2 Condicional (switch)
 
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = { 
     value: 'Tomar 3L de água por dia',
@@ -35,6 +35,38 @@ metas.push(
 )
 
 }
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+    message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false
+
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+
+     
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+    respostas.forEach((resposta) =>{
+    const meta = metas.find((m) => {
+        return m.value == resposta
+    })
+    meta.checked = true
+
+    })
+    console.log('meta(s) marcada como concluida(s)')
+}
+metas.push(
+    { value: meta, checked: false }
+
+)
 
 const start = async () => {
     
@@ -66,7 +98,7 @@ const start = async () => {
             console.log(metas)
             break
         case "listar":
-            console.log("vamos listar")
+            await listarMetas()
             break
         case "sair":
             console.log("Até a próxima pessoal!")
